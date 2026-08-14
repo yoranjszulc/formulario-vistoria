47ca2dead14cae669a7a9555411e2b960ff32839
yoranjszulc <yoranjs@gmail.com>
2026-08-14 20:38:16 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = "vistoria-v1";
+const ARQUIVOS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ARQUIVOS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(
+    ks.filter(k => k !== CACHE).map(k => caches.delete(k))
+  )).then(() => self.clients.claim()));
+});
+// navegação: tenta rede (pega atualização), cai no cache (offline na planta)
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request).then(r => {
+      const cp = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, cp));
+      return r;
+    }).catch(() => caches.match(e.request, {ignoreSearch: true})
+        .then(r => r || caches.match("./index.html")))
+  );
+});
